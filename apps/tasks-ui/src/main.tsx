144a98873a4b4fff5a09998136db722ceb167464
Akhil Joseph <ajpulluvelil@gmail.com>
2026-07-06 13:21:00 +0530

    MAJT-13 - Implementation of page not found component.

--- apps/tasks-ui/src/main.tsx
+++ apps/tasks-ui/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 
-import { ErrorBoundary } from './components/error-boundary/error-boundary/error-boundary';
+import { ErrorBoundary } from './components/error-wrappers/error-boundary/error-boundary';
 import { appRouter } from './routes/app-routes';
 
 const root = ReactDOM.createRoot(
