38995b2579d075a0f59a0bb0da8dd20991d6a9e1
Akhil Joseph <ajpulluvelil@gmail.com>
2026-08-04 15:14:29 +0530

    MAJT-34 - Navbar implementations.

--- apps/tasks-ui/src/layouts/navbar/navbar-layout.tsx
+++ apps/tasks-ui/src/layouts/navbar/navbar-layout.tsx
@@ -1,15 +1,35 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+// import styles from "./navbar-layout.module.scss";
 
 function NavbarLayout() {
     return (
-        <AppBar position="static" sx={{ width: "100%" }}>
-            <p>Collapse</p>
-            <p>Icon</p>
-            <p>Search</p>
+        <AppBar position="static" sx={{ width: "100%", display: "flex", flexDirection: "row" }}>
+
+            <Toolbar sx={{ display: "flex", justifyContent: "center", alignContent: "center", alignItems: "center" }}>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2, display: "flex", justifyContent: "center", alignContent: "center", alignItems: "center" }}
+                >
+                    <MenuIcon />
+                </IconButton>
+
+            </Toolbar>
+
+            <Typography variant='h4' sx={{ display: "flex", justifyContent: "center", alignContent: "center", alignItems: "center", fontWeight: 700 }}>
+                MajTasks
+            </Typography>
+
+            {/* <p>Search</p>
             <p>Notification</p>
             <p>Help</p>
             <p>Settings</p>
-            <p>Profile</p>
+            <p>Profile</p> */}
         </AppBar>
     )
 }
